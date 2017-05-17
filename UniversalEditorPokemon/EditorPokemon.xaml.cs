f8e9e6c9d59ce8408bf50a8467a5a6f68e62cf9c
Gabriel <tetradog@gmail.com>
2017-05-17 22:34:48 +0200

    subir archivos

---
+++ UniversalEditorPokemon/EditorPokemon.xaml.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 05/17/2017
+ * Hora: 21:35
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using PokemonGBAFrameWork;
+namespace UniversalEditorPokemon
+{
+	/// <summary>
+	/// Interaction logic for EditorPokemon.xaml
+	/// </summary>
+	public partial class EditorPokemon : UserControl
+	{
+		Pokemon pokemonActual;
+		RomData rom;
+		
+		public EditorPokemon()
+		{
+			InitializeComponent();
+		}
+
+		public RomData Rom {
+			get {
+				return rom;
+			}
+			set {
+				rom = value;
+			}
+		}
+
+		public Pokemon PokemonActual {
+			get {
+				return pokemonActual;
+			}
+			set {
+				Guardar();
+				pokemonActual = value;
+				PonPokemon();
+			}
+		}
+		public void Guardar()
+		{
+			if(Rom!=null&&PokemonActual!=null)
+				Pokemon.SetPokemon(Rom,PokemonActual);
+		}
+
+		void PonPokemon()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ UniversalEditorPokemon/Window1.xaml.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 17/05/2017
+ * Hora: 21:23
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Gabriel.Cat.Extension;
+using Microsoft.Win32;
+using PokemonGBAFrameWork;
+namespace UniversalEditorPokemon
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		RomData romCargada;
+		
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		void MiCargarRom_Click(object sender, RoutedEventArgs e)
+		{
+			Image imgPokemon;
+			OpenFileDialog openRom=new OpenFileDialog();
+			openRom.Filter="Pokemon GBA|*.gba";
+			
+			if(romCargada!=null&&MessageBox.Show("Quieres guardar los cambios?","Atención",MessageBoxButton.YesNo,MessageBoxImage.Question)==MessageBoxResult.Yes)
+				MiGuardarCambios_Click();
+			if(openRom.ShowDialog().GetValueOrDefault())
+			{
+				romCargada=new RomData(openRom.FileName);
+				
+				epPokemonActual.Rom=romCargada;
+				Title="Editando:\t"+romCargada.Rom.Nombre;
+				romCargada.Pokedex.Sort();
+				ugPokedex.Children.Clear();
+				for(int i=0;i<romCargada.Pokedex.Count;i++)
+				{
+					imgPokemon=new Image();
+					imgPokemon.Tag=romCargada.Pokedex[i];
+					imgPokemon.SetImage(romCargada.Pokedex[i].Sprites.SpritesFrontales);
+					imgPokemon.MouseLeftButtonUp+=PonPokemon;
+					ugPokedex.Children.Add(imgPokemon);
+					
+				}
+				epPokemonActual.PokemonActual=romCargada.Pokedex[0];
+			}else{
+				if(romCargada!=null)
+					MessageBox.Show("No se ha cambiado la rom");
+				else MessageBox.Show("No hay rom cargada!");
+			}
+		}
+
+		void PonPokemon(object sender, MouseButtonEventArgs e)
+		{
+			epPokemonActual.PokemonActual=((Image)sender).Tag as Pokemon;
+		}
+
+		void MiGuardarCambios_Click(object sender=null, RoutedEventArgs e=null)
+		{
+			epPokemonActual.Guardar();
+			try{
+				romCargada.Rom.Save();
+				MessageBox.Show("Se ha guardado correctamente","Todo esta bien",MessageBoxButton.OK,MessageBoxImage.Information);
+			}catch{
+				MessageBox.Show("No se ha podido guardar en el archivo actual...\n"+romCargada.Rom.BackUp(),"Se ha hecho un backup");
+				
+			}
+		}
+		void MiSobre_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Desarrollador: Pikachu240\nCreditos a los usuarios de Wahack que han hecho posible el desarrollo con su investigacion y tutoriales");
+		}
+	}
+}
